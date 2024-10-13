c863946a9caf53105f478528004e5c1640b10e45
Marc Altmann <marcaltmann@posteo.de>
2024-10-13 18:55:44 +0200

    Fix flake8 errors

--- app/core/filesystem.py
+++ app/core/filesystem.py
@@ -32,10 +32,10 @@ def remove_file(sender, **kwargs):
     print(kwargs)
 
     return
-    downloads_directory = user.profile.download_path()
-    file_path = downloads_directory / upload.filename
+    # downloads_directory = user.profile.download_path()
+    # file_path = downloads_directory / upload.filename
 
-    try:
-        file_path.unlink()
-    except FileNotFoundError:
-        print(f"File {upload.filename} does not exist.")
+    # try:
+    #     file_path.unlink()
+    # except FileNotFoundError:
+    #     print(f"File {upload.filename} does not exist.")
