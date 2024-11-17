74d26859d1d0f93d9dc683f74ca33b9171ae0a40
Marc Altmann <marcaltmann@posteo.de>
2024-11-17 22:57:51 +0800

    Small fix

--- app/core/views.py
+++ app/core/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 def welcome(request):
     user = request.user
-    upload_job_count = user.upload_jobs.count()
+    if user.is_authenticated:
+        upload_job_count = user.upload_jobs.count()
+    else:
+        upload_job_count = 0
 
     context = {"upload_job_count": upload_job_count, "download_file_count": 13}
     return render(request, "core/welcome.html", context)
