60ba7dc1dcb7a9c1545cf3507d2d12ea12d1a867
Marc Altmann <marcaltmann@posteo.de>
2024-11-18 23:11:00 +0800

    Try out pure JS approach

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -6,3 +6,21 @@ import UploadQueue from './upload_queue.js'
 const uploadQueueApp = createApp(UploadQueue)
 
 uploadQueueApp.mount('#upload-queue')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  getFormContent();
+})
+
+function getFormContent() {
+  const el = document.getElementById('upload-job-form');
+  el.addEventListener('submit', handleFormSubmit);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const csrfToken = form.elements[0].value;
+  const files = [...form.elements[1].files];
+  console.log(csrfToken, files);
+}
