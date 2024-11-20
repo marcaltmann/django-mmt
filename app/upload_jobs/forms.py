31995a5b4f580f3703997a5bd941c21f34ed9391
Marc Altmann <marcaltmann@posteo.de>
2024-11-20 21:15:09 +0800

    Revert "WIP languages"
    This reverts commit 0383c64c1c8a462219e57286d0111c005b6c91ef.

--- app/upload_jobs/forms.py
+++ app/upload_jobs/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from babel import Locale
 
 ACCEPTED_FILES = ["video/*", "audio/*", "image/*", "model/vnd.mts", "application/mxf"]
-
-locales = Locale.locales()
-locale_choices = [(locale_code, locale.get_display_name('en')) for locale_code, locale in locales.items() if len(locale_code) == 2]
 
 
 class MultipleFileInput(forms.ClearableFileInput):
@@ -24,7 +20,7 @@ class UploadJobForm(forms.Form):
     )
     language = forms.ChoiceField(
         label=_("Language"),
-        choices=locale_choices,
+        choices=[("en", _("English")), ("de", _("German"))],
         required=False,
     )
     make_available_on_platform = forms.BooleanField(
